88c796f03f2c34bda1366b265f956dc9c017315d
johanjro <johanjro.12@gmail.com>
2022-04-30 21:56:17 -0500

    Monto el Proyecto

---
+++ src/sistema/Sistema.java
@@ -0,0 +1,23 @@
+/*
+ Creación del paquete sistema
+ */
+package sistema;
+
+import sistema.empleadosGUI.frmEmpleados;
+
+/**
+ * Se implementa la clase sistema
+ * @author Daniel
+ */
+public class Sistema {
+
+    /**
+     * Argumentos generados a la clase principal por parte de netbeans
+     * @param args the command 
+     */
+    public static void main(String[] args) {
+        frmEmpleados f = new frmEmpleados();
+        f.setVisible (true);
+    }
+    
+}
